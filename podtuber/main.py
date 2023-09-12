dec675aa4edae831f7390a2b697fddb87182a42a
Zvika Haramaty <haramaty.zvika@gmail.com>
2023-09-12 14:52:13 +0300

    fixed notes

--- podtuber/main.py
+++ podtuber/main.py
@@ -7,13 +7,13 @@
 # TODO Mac's podcast takes 30 minutes to start playing (Daniel's report in Discord)
 
 # TODO Pocket Casts assumes next episode release time - why? how can we control this?
-# TODO Notes not showing (at least under Pocket Casts)
+
 
 from urllib.parse import urlparse
 import pytz
 import argparse
 
-from podgen import Podcast, Media, Person
+from podgen import Podcast, Media, Person, htmlencode
 from pytube import Playlist
 from pathvalidate import sanitize_filename
 
@@ -55,8 +55,8 @@ def create_rss_from_youtube_playlist(url):
 
             episode = podcast.add_episode()
             # print(clean_jpg_url(v.thumbnail_url))    #TODO use this for episodes as well?
-            episode.title = video.title
-            episode.summary = video.description
+            episode.title = htmlencode(video.title)
+            episode.summary = htmlencode(video.description)
             episode.publication_date = pytz.utc.localize(video.publish_date)  # TODO is it really UTC? always?
             episode.explicit = video.age_restricted
             stream = video.streams.get_audio_only()  # returns best mp4 audio stream
