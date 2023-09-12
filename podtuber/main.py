d47dbe414a19c717faae9f90ac2b498b6d8f072b
Zvika Haramaty <haramaty.zvika@gmail.com>
2023-09-12 14:31:07 +0300

    fixed video.description ; added problems TODOs at header

--- podtuber/main.py
+++ podtuber/main.py
@@ -1,5 +1,13 @@
-# TODO currently it issues a warning because of file extension mismatch
+# TODO currently it issues a warning because of file extension mismatch,
 # it works under Pocket Casts, might prove problematic for other apps
+# I guess it's the source of (most of) the following problems
+
+# TODO podcastindex.org doesn't play, or download
+# TODO https://www.castfeedvalidator.com/ gives warnings
+# TODO Mac's podcast takes 30 minutes to start playing (Daniel's report in Discord)
+
+# TODO Pocket Casts assumes next episode release time - why? how can we control this?
+# TODO Notes not showing (at least under Pocket Casts)
 
 from urllib.parse import urlparse
 import pytz
@@ -26,7 +34,7 @@ def create_rss_from_youtube_playlist(url):
         explicit=any([v.age_restricted for v in playlist.videos]),
         image=clean_jpg_url(playlist.videos[0].thumbnail_url),  # TODO make sure it's square, at least 1400x1400
         authors=[Person(playlist.owner)],
-        # TODO maybe set language from captions of first video, maybe from ytcfg fields - few have 'en'. need to investigate
+        # TODO maybe set language from ytcfg fields - few have 'en'. needs investigation
         language='en-US',
 
         # TODO support these?
@@ -39,19 +47,23 @@ def create_rss_from_youtube_playlist(url):
         try:
             video.check_availability()
         except Exception as err:
-            print(err)
-            continue
-        episode = podcast.add_episode()
-        # print(clean_jpg_url(v.thumbnail_url))    #TODO use this for episodes as well?
-        episode.title = video.title
-        episode.summary = video.description
-        episode.publication_date = pytz.utc.localize(video.publish_date)  # TODO is it really UTC? always?
-        episode.explicit = video.age_restricted
-        stream = video.streams.get_audio_only()  # returns best mp4 audio stream
-        episode.media = Media(stream.url, type='audio/x-m4a', size=stream.filesize)
-        episode.id = video.watch_url
-        episode.link = video.watch_url  # TODO is it helpful?
-        episode.authors = [Person(video.author)]
+            print(f"Skipping '{url}' because of: {err}")
+        else:
+            # make sure info is parsed (otherwise description might be None)
+            # taken from https://github.com/pytube/pytube/issues/1674
+            video.bypass_age_gate()
+
+            episode = podcast.add_episode()
+            # print(clean_jpg_url(v.thumbnail_url))    #TODO use this for episodes as well?
+            episode.title = video.title
+            episode.summary = video.description
+            episode.publication_date = pytz.utc.localize(video.publish_date)  # TODO is it really UTC? always?
+            episode.explicit = video.age_restricted
+            stream = video.streams.get_audio_only()  # returns best mp4 audio stream
+            episode.media = Media(stream.url, type='audio/x-m4a', size=stream.filesize)
+            episode.id = video.watch_url
+            episode.link = video.watch_url
+            episode.authors = [Person(video.author)]
 
     filename = f'{sanitize_filename(playlist.title)}.rss'.replace(' ', '_')
     podcast.rss_file(filename)
