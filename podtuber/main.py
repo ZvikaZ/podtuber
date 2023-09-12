cff37dfaab79d58b0b8eedc123b2345286a34b78
Zvika Haramaty <haramaty.zvika@gmail.com>
2023-09-12 18:15:31 +0300

    fixed error in case of missing description in Playlist ; fixed 'age_restricted' slowness

--- podtuber/main.py
+++ podtuber/main.py
@@ -4,7 +4,8 @@
 # TODO podcastindex.org doesn't play, or download
 # TODO Mac's podcast takes 30 minutes to start playing (Daniel's report in Discord)
 # TODO Pocket Casts assumes next episode release time - why? how can we control this?
-
+# TODO Google Podcast Index needs owner (i.e., mail) in order to index
+# TODO Google Podcast app can't add it manually - maybe also because of owner?
 
 from urllib.parse import urlparse
 from datetime import timedelta
@@ -25,21 +26,24 @@ def create_rss_from_youtube_playlist(url):
     print(playlist.title)
     assert playlist.videos
 
-    podcast = Podcast(
-        name=playlist.title,
-        description=playlist.description,
-        website=playlist.playlist_url,
-        explicit=any([v.age_restricted for v in playlist.videos]),
-        image=clean_jpg_url(playlist.videos[0].thumbnail_url),  # TODO make sure it's square, at least 1400x1400
-        authors=[Person(playlist.owner)],
-        # TODO maybe set language from ytcfg fields - few have 'en'. needs investigation
-        language='en-US',
+    podcast = Podcast()
+    podcast.name = playlist.title
+    try:
+        podcast.description = playlist.description
+    except KeyError:
+        podcast.description = playlist.title
+    podcast.website = playlist.playlist_url
+    podcast.explicit = playlist.videos[0].age_restricted
+    podcast.image = clean_jpg_url(playlist.videos[0].thumbnail_url)  # TODO make sure it's square, at least 1400x1400
+    podcast.authors = [Person(playlist.owner)]
 
-        # TODO support these?
-        # owner=Person(playlist.owner), # needs mail
-        # feed_url=...,
-        # category=...,
-    )
+    # TODO set automatically (e.g., https://github.com/pytube/pytube/issues/1742) or manually from argparse
+    podcast.language = 'en-US'
+
+    # TODO support these?
+    # podcast.owner=Person(playlist.owner)   # podcast.owner needs also mail (while playlist.owner is only name)
+    # podcast.feed_url=...
+    # podcast.category=...
 
     for video in playlist.videos:
         try:
@@ -57,6 +61,8 @@ def create_rss_from_youtube_playlist(url):
             episode.summary = htmlencode(video.description)
             episode.publication_date = pytz.utc.localize(video.publish_date)  # TODO is it really UTC? always?
             episode.explicit = video.age_restricted
+            if episode.explicit:
+                podcast.explicit = True
             stream = video.streams.get_audio_only()  # returns best mp4 audio stream
             episode.media = Media(stream.url,
                                   type='audio/x-m4a',
