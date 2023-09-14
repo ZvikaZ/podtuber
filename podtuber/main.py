9426e3b7939fb6ff67d529fa65606ab4a48598a4
Zvika Haramaty <haramaty.zvika@gmail.com>
2023-09-14 13:27:46 +0300

    refactored: splitted out YoutubeParser

--- podtuber/main.py
+++ podtuber/main.py
@@ -1,87 +1,70 @@
-# TODO Google Podcast Index (maybe) needs owner (i.e., mail) in order to index
 # TODO Pocket Casts assumes next episode release time - why? how can we control this?
 
 # I'm not sure that those are still relevant, they're from before I started downloading from youtube:
 # TODO podcastindex.org doesn't play, or download
 # TODO Mac's podcast takes 30 minutes to start playing (Daniel's report in Discord)
 
-from urllib.parse import urlparse, quote
-from pathlib import Path
-import pytz
 import sys
-
-from podgen import Podcast, Media, Person, Category, htmlencode
-from pytube import Playlist
-from pathvalidate import sanitize_filename
 import tomli
+from urllib.parse import urlparse
+
+from podgen import Podcast, Person, Category, htmlencode
+from pathvalidate import sanitize_filename
+
+from youtube_parser import YoutubeParser
 
 
-def clean_jpg_url(url):
-    return urlparse(url)._replace(query='').geturl()
+def get_parser(url):
+    if urlparse(url).netloc == 'www.youtube.com' and urlparse(url).path == '/playlist':
+        return YoutubeParser(url)
+    else:
+        print('Unsupported playlist: ', url)
+        sys.exit(
+            'Currently only YouTube playlists are supported. You can open an issue, maybe your parser will be added.')
 
 
-def get_media_from_youtube(config, series_title, stream):
-    path = Path('files') / series_title
-    path.mkdir(parents=True, exist_ok=True)
-    stream.subtype = 'm4a'
-    file = Path(stream.download(output_path=path))
-    media = Media(
-        url=f'{config["general"]["base_url"]}/{quote((path / file.name).as_posix())}',
-        size=stream.filesize,
-    )
-    media.populate_duration_from(file)
-    return media
+def create_rss(podcast_config, config):
+    parser = get_parser(podcast_config['url'])
+    print(parser.get_name())
 
-
-def create_rss_from_youtube_playlist(podcast_config, config):
-    playlist = Playlist(podcast_config['url'])
-    print(playlist.title)
-    assert playlist.videos
-
-    sanitized_title = sanitize_filename(playlist.title).replace(' ', '_')
+    sanitized_title = sanitize_filename(parser.get_name()).replace(' ', '_')
     rss_filename = f'{sanitized_title}.rss'
 
     podcast = Podcast()
-    podcast.name = playlist.title
-    try:
-        podcast.description = playlist.description
-    except KeyError:
-        podcast.description = playlist.title
-    podcast.website = playlist.playlist_url
-    podcast.explicit = playlist.videos[0].age_restricted
-    podcast.image = clean_jpg_url(playlist.videos[0].thumbnail_url)  # TODO make sure it's square, at least 1400x1400
-    podcast.authors = [Person(playlist.owner)]
+    podcast.name = parser.get_name()
+    podcast.description = parser.get_description()
+    podcast.website = parser.get_website()
+    podcast.explicit = False  # will be updated if one of the episodes is True
+    podcast.image = parser.get_image()
+    podcast.authors = parser.get_authors()
+
     podcast.category = Category(podcast_config.get('category'), podcast_config.get('subcategory'))
     podcast.feed_url = f'{config["general"]["base_url"].strip("/")}/{rss_filename}'
     if podcast_config.get('owner_mail'):
-        podcast.owner = Person(playlist.owner, podcast_config.get('owner_mail'))
+        podcast.owner = Person(parser.get_owner_name(), podcast_config.get('owner_mail'))
 
     # TODO set automatically (e.g., https://github.com/pytube/pytube/issues/1742)
     podcast.language = podcast_config.get('language')
 
-    for video in playlist.videos:
+    for parsed_episode in parser.get_episodes():
         try:
-            video.check_availability()
+            parsed_episode.check_availability()
         except Exception as err:
-            print(f"Skipping '{playlist.playlist_url}' because of: {err}")
+            print(f"Skipping '{parsed_episode.get_title()}' ({parsed_episode.get_link()}) because of: {err}")
         else:
-            # make sure info is parsed (otherwise description might be None)
-            # taken from https://github.com/pytube/pytube/issues/1674
-            video.bypass_age_gate()
-
             episode = podcast.add_episode()
             # print(clean_jpg_url(v.thumbnail_url))    #TODO use this for episodes as well?
-            episode.title = htmlencode(video.title)
-            episode.summary = htmlencode(video.description)
-            episode.publication_date = pytz.utc.localize(video.publish_date)  # TODO is it really UTC? always?
-            episode.explicit = video.age_restricted
+            episode.title = htmlencode(parsed_episode.get_title())
+            episode.summary = htmlencode(parsed_episode.get_summary())
+            episode.publication_date = parsed_episode.get_publication_date()
+            episode.explicit = parsed_episode.get_explicit()
             if episode.explicit:
                 podcast.explicit = True
-            stream = video.streams.get_audio_only()  # returns best mp4 audio stream
-            episode.media = get_media_from_youtube(config, sanitized_title, stream)
-            episode.id = video.watch_url
-            episode.link = video.watch_url
-            episode.authors = [Person(video.author)]
+            episode.media = parsed_episode.get_media(base_url=config["general"]["base_url"],
+                                                     series_title=sanitized_title)
+            episode.id = parsed_episode.get_id()
+            episode.link = parsed_episode.get_link()
+            episode.authors = parsed_episode.get_authors()
 
     podcast.rss_file(rss_filename)
     return rss_filename
@@ -100,5 +83,5 @@ if __name__ == '__main__':
             f'Illegal config.toml file. You can use https://github.com/zvikaZ/podtuber/config.toml as a reference.')
 
     for podcast_config in config.get('podcasts'):
-        rssfile = create_rss_from_youtube_playlist(podcast_config, config)
+        rssfile = create_rss(podcast_config, config)
         print(f"Created '{rssfile}'\n")
